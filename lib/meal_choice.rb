0295a05d4209d84a160e8ed4e7400a9b20bbdf68
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 15:06:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,9 @@
 # Your code here
+def meal_choice(veg1, veg2, protein = "meat")
+    message = "What a nutritious meal!" 
+    puts = message + "A plate of #{protein} with #{veg1} and #{veg2}."
+    #return "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+ 
+ meal_choice("broccoli", "macaroni")
+ meal_choice("broccoli", "macaroni", "tofu")
